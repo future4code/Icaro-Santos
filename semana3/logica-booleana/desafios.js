1b7450c305ab26f637dd18885e22a76cb16672a9
icarohadouken <icaro.ssantos96@gmail.com>
2020-03-30 15:00:10 -0300

    Finalizado exerícios obrigatórios e desafios

---
+++ semana3/logica-booleana/desafios.js
@@ -0,0 +1,42 @@
+//a
+let lb = 20;
+
+let kg = lb / 2.205;
+
+console.log(lb + "lb equivalem a " + kg + "kg");
+
+//b
+let oz = 10.5;
+
+kg = oz / 35.274;
+
+console.log(oz + "oz equivalem a " + kg + "kg");
+
+//c
+let mi = 100;
+let m = mi * 1609;
+
+console.log(mi + "mi equivalem a " + m + "m");
+
+//d
+let ft = 50;
+m = ft / 3.281;
+
+console.log(ft + "ft equivalem a " + m + "m");
+
+//e
+let gal = 103.56;
+let l = gal * 3.785;
+
+console.log(gal + "gal equivalem a " + l + "l");
+
+//f
+let xic = 450;
+l = xic * 4.23;
+
+console.log(xic + "xic equivalem a " + l + "l");
+
+//g
+xic = prompt("Digite a quantidade de xícaras que deseja converter");
+l = xic * 4.23;
+console.log(xic + "xic equivalem a " + l + "l");
---
+++ semana3/logica-booleana/escrita2.js
@@ -0,0 +1,11 @@
+let nome = prompt("Qual seu nome");
+let idade = prompt("Qual sua idade");
+let rua = prompt("Qual sua rua");
+let animais = prompt("Quantos animais você tem em casa");
+let estudo = prompt("O que você estuda");
+
+console.log("Qual seu nome?\n" + nome + 
+"\n\nQual sua idade?\n" + idade + 
+"\n\nQual sua rua?\n" + rua + 
+"\n\nQuantos animais você tem em casa?\n" + animais + 
+"\n\nO que você estuda?\n" + estudo);
