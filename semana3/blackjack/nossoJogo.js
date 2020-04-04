5143b2f073342821b312432f669b9782b3048120
icarohadouken <icaro.ssantos96@gmail.com>
2020-04-04 14:18:49 -0300

    finalizado o exercício obrigatório

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -1,3 +1,33 @@
+console.log("Bem vindo ao jogo de blackjack!");
+
+if(confirm("Quer iniciar uma rodada?")){
+   let carta = comprarCarta();
+   const carta1Usuario = carta;
+   carta = comprarCarta();
+   const carta2Usuario = carta;
+
+   carta = comprarCarta();
+   const carta1PC = carta;
+   carta = comprarCarta();
+   const carta2PC = carta;
+   
+   const valorUsuario = carta1Usuario.valor + carta2Usuario.valor;
+   const valorPC = carta1PC.valor + carta2PC.valor;
+
+   console.log("Usuário - Cartas " + carta1Usuario.texto + " " + carta2Usuario.texto + " - pontuação " + valorUsuario);
+   console.log("Computador - Cartas " + carta1PC.texto + " " + carta2PC.texto + " - pontuação " + valorPC);
+
+   if (valorUsuario > valorPC){
+      console.log("O Usuário Ganhou!");
+   }else if(valorPC > valorUsuario){
+      console.log("O Computador ganhou!");
+   }else{
+      console.log("Empate!");
+   }
+}else{
+   console.log("O jogo acabou");
+}
+
 /**
  * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
  * 
