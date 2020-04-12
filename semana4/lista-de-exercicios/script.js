e21bd398fe3c4781113e502b10c7bb4bccf365ae
icarohadouken <icaro.ssantos96@gmail.com>
2020-04-12 13:09:17 -0300

    Finalizado exercício 01 de lógica de programação da aula 14

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -4,3 +4,11 @@
 // 2. Primeira mensagem seria 165. Segunda não apareceria nada, somente um alert dizendo TIPO DE INVESTIMENTO INFORMADO INCORRETO!
 // 3. Quantidade total de números 14. Depois somente os números pares que seriam 6 numeros. Depois impares 8.
 // 4. Infinity. E no segundo console.log seria 1590.
+
+// Lógica de programação
+// Exercício 1
+// a. false
+// b. true
+// c. true
+// d. true
+// e. true
