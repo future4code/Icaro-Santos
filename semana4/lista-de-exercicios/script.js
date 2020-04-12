6e8ef83a56e5ae8a9fa404bf81aa7320616a5c09
icarohadouken <icaro.ssantos96@gmail.com>
2020-04-12 13:05:18 -0300

    Finalizado Leitura de códigos da aula 14

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,6 @@
+// Aula 14 - DOM
+// Leitura de código
+// 1. Digamos que a cotação seja 4,2. Então o valor mostrado no console seria 420;
+// 2. Primeira mensagem seria 165. Segunda não apareceria nada, somente um alert dizendo TIPO DE INVESTIMENTO INFORMADO INCORRETO!
+// 3. Quantidade total de números 14. Depois somente os números pares que seriam 6 numeros. Depois impares 8.
+// 4. Infinity. E no segundo console.log seria 1590.
