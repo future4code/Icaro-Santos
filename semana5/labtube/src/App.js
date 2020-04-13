9e28063a9665fc2affdfca4130426a132cac0104
icarohadouken <icaro.ssantos96@gmail.com>
2020-04-13 15:52:05 -0300

    Adicionada tag para canal, views e horário enviado

--- semana5/labtube/src/App.js
+++ semana5/labtube/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 
 function App() {
   const titulo = "Título do vídeo";
+  const canal = "Canal";
+  const views = 100;
+  const dataEnviada = "há " + 4 + " horas";
 
   const reproduzVideo = () =>{
     alert("O vídeo está sendo reproduzido");
@@ -33,34 +36,50 @@ function App() {
                   <div className="box-pagina-principal media1" onClick={reproduzVideo}>
                       <img src="https://picsum.photos/400/400?a=1 " alt="" />
                       <h4>{titulo}</h4>
+                      <h5>{canal}</h5>
+                      <p>{views} Visualizações . {dataEnviada}</p>
                   </div>
                   <div className="box-pagina-principal media2" onClick={reproduzVideo}>
                       <img src="https://picsum.photos/400/400?a=2 " alt="" />
                       <h4>{titulo}</h4>
+                      <h5>{canal}</h5>
+                      <p>{views} Visualizações . {dataEnviada}</p>
                   </div>
                   <div className="box-pagina-principal media3" onClick={reproduzVideo}>
                       <img src="https://picsum.photos/400/400?a=3 " alt="" />
                       <h4>{titulo}</h4>
+                      <h5>{canal}</h5>
+                      <p>{views} Visualizações . {dataEnviada}</p>
                   </div>
                   <div className="box-pagina-principal media4" onClick={reproduzVideo}>
                       <img src="https://picsum.photos/400/400?a=4 " alt="" />
                       <h4>{titulo}</h4>
+                      <h5>{canal}</h5>
+                      <p>{views} Visualizações . {dataEnviada}</p>
                   </div>
                   <div className="box-pagina-principal media5" onClick={reproduzVideo}>
                       <img src="https://picsum.photos/400/400?a=5 " alt="" />
                       <h4>{titulo}</h4>
+                      <h5>{canal}</h5>
+                      <p>{views} Visualizações . {dataEnviada}</p>
                   </div>
                   <div className="box-pagina-principal media6" onClick={reproduzVideo}>
                       <img src="https://picsum.photos/400/400?a=6 " alt="" />
                       <h4>{titulo}</h4>
+                      <h5>{canal}</h5>
+                      <p>{views} Visualizações . {dataEnviada}</p>
                   </div>
                   <div className="box-pagina-principal media7" onClick={reproduzVideo}>
                       <img src="https://picsum.photos/400/400?a=7 " alt="" />
                       <h4>{titulo}</h4>
+                      <h5>{canal}</h5>
+                      <p>{views} Visualizações . {dataEnviada}</p>
                   </div>
                   <div className="box-pagina-principal media8" onClick={reproduzVideo}>
                       <img src="https://picsum.photos/400/400?a=8 " alt="" />
                       <h4>{titulo}</h4>
+                      <h5>{canal}</h5>
+                      <p>{views} Visualizações <dot></dot> {dataEnviada}</p>
                   </div>
               </section>
           </main>
