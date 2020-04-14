fef31dedebe9e033580667807ea2570e67bb38eb
icarohadouken <icaro.ssantos96@gmail.com>
2020-04-14 17:03:25 -0300

    Adicionando atributo Alt nas imagens

--- semana5/labedin/src/components/CardPequeno/CardPequeno.js
+++ semana5/labedin/src/components/CardPequeno/CardPequeno.js
@@ -4,7 +4,7 @@ import './CardPequeno.css';
 function CardPequeno(props){
     return (
         <div className="smallcard-container">
-            <img src={props.imagem}/>
+            <img src={props.imagem} alt={props.altImagem}/>
             <span><b>{props.descricao}</b> {props.informacao}</span>
         </div>
     );
