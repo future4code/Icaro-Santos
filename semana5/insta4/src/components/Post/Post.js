15363ed150a60d93575e4d5135f4ee16760c0013
icarohadouken <icaro.ssantos96@gmail.com>
2020-04-16 16:13:37 -0300

    finalizada primeira parte dos desafios

--- semana5/insta4/src/components/Post/Post.js
+++ semana5/insta4/src/components/Post/Post.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Post.css'
-
 import {IconeComContador} from '../IconeComContador/IconeComContador'
 import iconeMarcacaoBranco from '../../img/bookmark_white.svg'
 import iconeMarcacaoPreto from '../../img/bookmark.svg'
@@ -11,6 +10,38 @@ import iconeCompartilhar from '../../img/share.svg'
 import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
 import {IconeSemContador} from '../IconeSemContador/IconeSemContador'
 import {SecaoCompartilhar} from '../SecaoCompartilhar/SecaoCompartilhar'
+import styled from 'styled-components'
+
+const PostContainer = styled.div`
+  border: 1px solid gray;
+  width: 300px;
+  margin-bottom: 10px;
+`
+
+const PostHeader = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`
+
+const PostFooter = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+`
+const FotoUsuario = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+`
+
+const FotoPost = styled.img`
+  width: 100%;
+`
 
 class Post extends React.Component {
   state = {
@@ -119,15 +150,15 @@ class Post extends React.Component {
         mensagem={this.state.mensagem}/>
     }
 
-    return <div className={'post-container'}>
-      <div className={'post-header'}>
-        <img className={'user-photo'} src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+    return <PostContainer>
+      <PostHeader>
+        <FotoUsuario className={'user-photo'} src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
         <p>{this.props.nomeUsuario}</p>
-      </div>
+      </PostHeader>
 
-      <img className={'post-photo'} src={this.props.fotoPost} alt={'Imagem do post'}/>
+      <FotoPost className={'post-photo'} src={this.props.fotoPost} alt={'Imagem do post'}/>
 
-      <div className={'post-footer'}>
+      <PostFooter>
         <IconeComContador
           icone={iconeCurtida}
           onClickIcone={this.onClickCurtida}
@@ -149,10 +180,10 @@ class Post extends React.Component {
           onClickIcone={this.onClickComentario}
           valorContador={this.state.numeroComentarios}
         />
-      </div>
+      </PostFooter>
       {componenteComentario}
       {componenteCompartilhar}
-    </div>
+    </PostContainer>
   }
 }
 
