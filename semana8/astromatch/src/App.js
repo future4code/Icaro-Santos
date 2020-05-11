5f2e89bc13e46a2ca0dab312c8f6cfad5b66f057
icarohadouken <icaro.ssantos96@gmail.com>
2020-05-11 15:15:29 -0300

    Finalizado esqueleto básico

---
+++ semana8/astromatch/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PrincipalScreen from './Containers/PrincipalScreen'
+
+function App() {
+  return (
+    <div className="App">
+      <PrincipalScreen></PrincipalScreen>
+    </div>
+  );
+}
+
+export default App;
---
+++ semana8/astromatch/src/Containers/PrincipalScreen/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+
+function PrincipalScreen() {
+
+    return (
+        <div>
+            <Card>
+                <CardContent>
+                    Testando123
+                </CardContent>
+                <CardContent>
+                    Testando123
+                </CardContent>
+                <CardActions>
+                    Sou lindo
+                </CardActions>
+            </Card>
+        </div>
+    )
+
+}
+
+export default PrincipalScreen
