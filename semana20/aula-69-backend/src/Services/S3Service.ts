e55392753da22a07b40bccfed5b57ed55d32cb42
icarohadouken <icaro.ssantos96@gmail.com>
2020-08-07 02:03:25 -0300

    arrumei o problema que o github estava acusando de falha de segurança do código

--- semana20/aula-69-backend/src/Services/S3Service.ts
+++ semana20/aula-69-backend/src/Services/S3Service.ts
@@ -2,8 +2,8 @@ import {S3} from 'aws-sdk'
 
 export class S3Service {
     private s3 = new S3({
-        accessKeyId: "AKIAI6R27TYX6JAAZCRQ",
-        secretAccessKey: "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"
+        accessKeyId: process.env.AWS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY
     })
 
     public async uploadFile(input: UploadFileInput): Promise<UploadFileOutput> {
