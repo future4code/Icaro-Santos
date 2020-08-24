1cb5f25caf34da3e36ba1c65b5b45e7250483890
icarohadouken <icaro.ssantos96@gmail.com>
2020-08-24 17:12:13 -0300

    finalizados exercícios

---
+++ semana23/Aula 72/src/index.ts
@@ -0,0 +1,49 @@
+const isOneEddit = (input1: string, input2: string): boolean => {
+    if(input1.length > input2.length + 1 || input1.length < input2.length - 1){
+        return false
+    }
+
+    let countingLetters = 0
+
+    for(const char of input2){
+        if(input1.indexOf(char) !== -1){
+            countingLetters++
+        }
+
+        console.log(countingLetters)
+    }
+
+    if(countingLetters <= input1.length + 1 && countingLetters >= input1.length - 1){
+        return true
+    }
+
+    return false
+}
+
+const compressingChar = (input: string): string => {
+    const strings: string[] = []
+    let savingChar = input[0]
+    let countingLetters = 0
+
+    for(const char of input){
+        if (char !== savingChar){
+            strings.push(savingChar + countingLetters)
+            savingChar = char
+            countingLetters = 0
+        }
+        countingLetters++
+    }
+
+    strings.push(savingChar + countingLetters)
+    let result = ''
+    for(const char of strings){
+        result += char
+    }
+
+    if(result.length > input.length){
+        return input
+    }
+    return result
+}
+
+console.log(compressingChar("escola"))
